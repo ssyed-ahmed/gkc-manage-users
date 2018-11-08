bd2141c8c1a99fbcf18203671e2be6797cafdd68
unknown <sumair_ahmed4@yahoo.co.in>
2018-11-08 18:49:57 +1100

    Added initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+        public username: string,
+        public firstName: string,
+        public lastName: string,
+        public interests: string[],
+        public phoneNumber: number
+    ){}
+}
