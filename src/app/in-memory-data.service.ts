e94055f079792a4e431223f8d0deb26241a939db
unknown <sumair_ahmed4@yahoo.co.in>
2018-11-10 17:50:53 +1100

    Added more dummy users

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -16,7 +16,9 @@ export class InMemoryDataService implements InMemoryDbService {
       new User(3, 'dougcharles', 'Doug', 'Charles', ['Cooking', 'Basketball'], 347979595, 'https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg'),
       new User(4, 'patrickbuck', 'Patrick', 'Buck', ['Football', 'Cycling', 'DIY stuff'], 535773733, 'https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg'),
       new User(5, 'rajmathur', 'Raj', 'Mathur', ['Web development', 'IT news'], 326634099, 'https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg'),
-      new User(6, 'stephencoin', 'Stephen', 'Coin', ['Photography', 'Gardening'], 870074444, 'https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg')
+      new User(6, 'stephencoin', 'Stephen', 'Coin', ['Photography', 'Gardening'], 870074444, 'https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg'),
+      new User(7, 'richard_butler','Richard', 'Butler', ['Swimming', 'Fencing', 'Horse racing', 'Boating'], 477537389, 'https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg'),
+      new User(8, 'greenhorne', 'Green', 'Horne', ['Shooting', 'Archery'], 868383553, 'https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg')
     ];
 
     return {users};
